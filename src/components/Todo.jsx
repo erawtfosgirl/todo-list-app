98be7a89429e07378d457fecd64ed947d58fcaf5
vjuggler <vefaferzeliyeva11@gmail.com>
2024-01-12 13:14:02 +0400

    delete todo

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,13 +3,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
 
 
-export const Todo = ({ todo, toggleComplete }) => {
+export const Todo = ({ todo, toggleComplete, deleteTodo }) => {
     return (
         <div className='todo'>
             <p onClick={() => toggleComplete(todo.id)} className={`${todo.completed ? 'completed' : 'incompleted'}`}>{todo.task}</p>
             <div>
                 <FontAwesomeIcon className="edit-icon" icon={faPenToSquare} />
-                <FontAwesomeIcon className="delete-icon" icon={faTrash} />
+                <FontAwesomeIcon className="delete-icon" icon={faTrash} onClick={() => deleteTodo(todo.id)} />
             </div>
         </div>
     )
--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { TodoForm } from './TodoForm'
 import { Todo } from './Todo'
 import { v4 as uuidv4 } from 'uuid'
+
 uuidv4();
 
 export const TodoWrapper = () => {
@@ -14,6 +15,9 @@ export const TodoWrapper = () => {
         ]);
         console.log(todos);
     }
+
+    const deleteTodo = (id) => setTodos(todos.filter((todo) => todo.id !== id));
+
 
     const toggleComplete = (id) => {
         setTodos(
@@ -31,6 +35,7 @@ export const TodoWrapper = () => {
                     key={index}
                     todo={todo}
                     toggleComplete={toggleComplete}
+                    deleteTodo={deleteTodo}
                 />
             ))}
         </div>
